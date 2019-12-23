f1fb9d700a7949d121bf75adff8e02b28d69d455
Krishnan Subramanian <skrish79@gmail.com>
2019-12-23 09:48:26 +0530

    added comparison check for None condition in search module

--- shconfparser/search.py
+++ shconfparser/search.py
@@ -8,12 +8,14 @@ class Search:
 
     def search_in_tree(self, pattern, data=None):
         if data == None: return None
+        if data == "None": return None
         pattern = re.compile(pattern)
         for key in data.keys():
             if pattern.match(key): return pattern.match(key)
 
     def search_all_in_tree(self, pattern, data=None):
         if data == None: return None
+        if data == "None": return None
         pattern = re.compile(pattern)
         match = OrderedDict()
         for key in data.keys():
@@ -23,12 +25,14 @@ class Search:
 
     def search_in_table(self, pattern, data=None, header_column=None):
         if data == None: return None
+        if data == "None": return None
         pattern = re.compile(pattern)
         for each_row in data:
             if pattern.match(each_row[header_column]): return each_row
 
     def search_all_in_table(self, pattern, data=None, header_column=None):
         if data == None: return None
+        if data == "None": return None
         pattern = re.compile(pattern)
         match = []
         for each_row in data:
